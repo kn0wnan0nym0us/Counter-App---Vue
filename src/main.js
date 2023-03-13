82873a88fe27e003703337bac94c45155a48420a
Emmanuel Oluwafemi Oluwagbemi <68505139+kn0wnan0nym0us@users.noreply.github.com>
2023-03-13 10:32:52 +0100

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles.css'
+
 
 const app = createApp(App)
 app.use(store)
