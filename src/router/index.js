97b3e17c10a55f23bd89edb0cf3b47e0517de360
Emmanuel Oluwafemi Oluwagbemi <68505139+kn0wnan0nym0us@users.noreply.github.com>
2023-03-13 09:47:22 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Counter from '../components/Counter.vue'
 import NotFound from '../components/NotFound.vue'
+import Home from '../components/Home.vue'
 
 const routes = [
   {
     path: '/counter',
     name: 'Counter',
     component: Counter
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
   },
   {
     path: '/:catchAll(.*)',
