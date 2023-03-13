79b02edf5dd508fbe788adbf5b9c876f0853b053
Emmanuel Oluwafemi Oluwagbemi <68505139+kn0wnan0nym0us@users.noreply.github.com>
2023-03-13 10:34:39 +0100

    Add files via upload

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,23 @@
 <template>
-    <div class="home">
-    </div>
-  </template>
-  
+  <div class="home">
+    <h1>Welcome to Home!</h1>
+    <router-link to="/counter">
+      <button class="counter-button">Counter</button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import Counter from '../components/Counter.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Counter,
+  },
+};
+</script>
+
   <style>
   .home {
     display: flex;
